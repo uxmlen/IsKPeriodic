7bf151a84fbb4f3cf7afde602ca609aa25ee0306
uxmlen <38371923+uxmlen@users.noreply.github.com>
2023-04-17 23:12:50 +0300

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
-int main(void)
+bool IsKPeriodic(const std::string& str, int k)
 {
-    printf("hellow");
+    if (str.size() % k || k <= 0 || k >= str.size())
+        return false;
+
+    for (size_t i = 0; i < str.size() - k; ++i)
+        if (str[i] != str[i + k])
+            return false;
+
+    return true;
+}
+
+int main()
+{
+    std::cout << (IsKPeriodic("abcabcabc", 3) ? "yes" : "no") << std::endl;
+    std::cout << (IsKPeriodic("abcdabcdabcd", 4) ? "yes" : "no") << std::endl;
+    std::cout << (IsKPeriodic("abcabc", 5) ? "yes" : "no") << std::endl;
+    std::cout << (IsKPeriodic("abcabcaaa", 1) ? "yes" : "no") << std::endl;
+    std::cout << (IsKPeriodic("abcabcqwr", 1) ? "yes" : "no") << std::endl;
+    std::cout << (IsKPeriodic("abdabdabd", 3) ? "yes" : "no") << std::endl;
+
     return 0;
 }
